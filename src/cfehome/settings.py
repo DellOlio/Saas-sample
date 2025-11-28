3876d1e97bf985c42d25136cde9eaa15f0444e55
Marin DellOlio <mmarin476@gmail.com>
2025-11-28 14:47:01 +0100

    debug i secret

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os 
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,11 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-prtbww+n=m*!nc)+ckpo_0*3d*nd41za&w3i!fdc^3x36=3%ei'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DEBUG")).lower() == "true" 
+# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true" 
 
+DEBUG = config("DJANGO_DEBUG", cast=bool)
+
+print("Debug",DEBUG, type(DEBUG))
 ALLOWED_HOSTS = [
     ".railway.app"
 ]
