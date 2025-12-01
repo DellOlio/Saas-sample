139af0f574833c8ab81b80038de320bfc5ecd0e8
Marin DellOlio <mmarin476@gmail.com>
2025-12-01 10:25:36 +0100

    vendor_pull

---
+++ src/commando/management/commands/vendor_pull.py
@@ -0,0 +1,15 @@
+import helpers
+
+from typing import Any
+from django.core.management.base import BaseCommand
+
+VENDOR_STATICFILES = {
+    "flowbite.min.css" : "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css",
+    "flowbite.min.js": "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js",
+}
+
+class Command(BaseCommand):
+    def handle(self, *args: Any, **options: Any):
+        print("Hello World")
+        for name,url in VENDOR_STATICFILES.items():
+            print(name,url)
